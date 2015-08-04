1f07e93df7bc3063d98117a26444cb6f4aa4238f
Nathan Chapman <nathan@nchp.mn>
2015-08-04 20:26:14 +1000

    Add simple about structure

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -1,4 +1,11 @@
-<section id="about-me">
-  <h1><?php echo $data->title()->html() ?></h1>
-  <?php echo $data->text()->kirbytext() ?>
+<section id="about-me" class="pure-g">
+    <?php $background = $data->photo()->toFile(); ?>
+
+    // Use the file object
+    echo $file->url();
+    <div class="pure-u-md-1-3" style="background-image:url('fjf')"></div>
+    <div class="pure-u-md-2-3">
+        <h1><?php echo $data->title() ?></h1>
+        <?php echo $data->text()->kirbytext() ?>
+    </div>
 </section>
