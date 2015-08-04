b53221bb10354b221c56d90c4c773ca188c79437
Nathan Chapman <nathan@nchp.mn>
2015-08-04 21:02:04 +1000

    Basic styling

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -1,10 +1,10 @@
-<section id="about-me" class="pure-g">
+<section id="about" class="pure-g">
     <?php $background = $data->photo()->toFile(); ?>
-
-    // Use the file object
-    echo $file->url();
-    <div class="pure-u-md-1-3" style="background-image:url('fjf')"></div>
-    <div class="pure-u-md-2-3">
+    <!--div class="pure-u-md-1-3 img-container" style="background-image:url(<?php echo $background->url() ?>)"></div-->
+    <div class="pure-u-1 pure-u-md-1-3">
+        <img src="<?php echo $background->url() ?>" class="img-responsive" />
+    </div>
+    <div class="pure-u-1 pure-u-md-2-3">
         <h1><?php echo $data->title() ?></h1>
         <?php echo $data->text()->kirbytext() ?>
     </div>
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,7 +9,7 @@
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
-  <?php echo css('assets/css/purecss.min.css') ?>
+  <?php echo css('modules/purecss/pure.min.css') ?>
   <?php echo css('assets/css/main.min.css') ?>
 
 </head>
