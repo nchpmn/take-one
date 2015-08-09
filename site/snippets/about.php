5cf540bbd20ff021497be9676cfe3ab8f8574ce7
Nathan Chapman <nathan@nchp.mn>
2015-08-09 17:54:22 +1000

    Revert af9d5fc..123ded1
    This rolls back to commit af9d5fce811be84a57fe3d732dd82187c968bef3.

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -1,10 +1,4 @@
-<section class="pure-g img-bg-parent">
-    <?php // Convert the filename to a full file object
-    $file = $data->photo()->toFile(); ?>
-    <div class="pure-u-1 pure-u-sm-1-2 img-bg" style="background-image:url(<?php echo $file->url() ?>)">
-    </div>
-    <div class="pure-u-1 pure-u-sm-1-2 offset-sm-1-2 l-box-lg">
-            <h1><?php echo $data->title() ?></h1>
-            <?php echo $data->text()->kirbytext() ?>
-    </div>
+<section id="about-me">
+  <h1><?php echo $data->title()->html() ?></h1>
+  <?php echo $data->text()->kirbytext() ?>
 </section>
