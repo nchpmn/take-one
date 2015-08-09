0f6ab0a84da4c75bf1c2dd32378f268b89fc2796
Nathan Chapman <nathan@nchp.mn>
2015-08-09 17:56:41 +1000

    Add custom Bootstrap assets

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -9,14 +9,8 @@
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
-  <?php echo css('assets/css/main.css') ?>
+  <?php echo css('modules/bootstrap/css/bootstrap.min.css') ?>
+  <?php echo css('assets/css/main.min.css') ?>
 
 </head>
 <body>
-
-  <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
-    <?php snippet('menu') ?>
-  </header>
