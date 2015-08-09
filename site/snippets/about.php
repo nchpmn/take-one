359091f3025fffb3e7e9a0b1c2224bde4471b4bf
Nathan Chapman <nathan@nchp.mn>
2015-08-09 17:22:59 +1000

    Add php to call image from Kirby

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -1,18 +1,10 @@
 <section class="pure-g img-bg-parent">
-    <div class="pure-u-1 pure-u-sm-1-2 img-bg" style="background-image:url(http://www.raindance.org/wp-content/uploads/film-set2-780x398.jpg)">
+    <?php // Convert the filename to a full file object
+    $file = $data->photo()->toFile(); ?>
+    <div class="pure-u-1 pure-u-sm-1-2 img-bg" style="background-image:url(<?php echo $file->url() ?>)">
     </div>
     <div class="pure-u-1 pure-u-sm-1-2 offset-sm-1-2 l-box-lg">
             <h1><?php echo $data->title() ?></h1>
             <?php echo $data->text()->kirbytext() ?>
-    </div>
-</section>
-<section class="pure-g">
-    <div class="pure-u-3-4 l-box-lg">
-        <p>You know the worst thing about being a slave? They make you work, but they don't pay you or let you go. And I'm his friend Jesus. No, I'm Santa Claus! Would you censor the Venus de Venus just because you can see her spewers? You can see how I lived before I met you. Wow, you got that off the Internet? In my day, the Internet was only used to download pornography.</p>
-<p>Spare me your space age technobabble, Attila the Hun! I don't know what you did, Fry, but once again, you screwed up! Now all the planets are gonna start cracking wise about our mamas. Are you crazy? I can't swallow that. Oh no! The professor will hit me! But if Zoidberg 'fixes' it&hellip; then perhaps gifts! Oh yeah, good luck with that. Ask her how her day was.</p>
-    </div>
-    <div class="pure-u-1-4 l-box">
-    <p>Ah, now we see the violence inherent in the system! Who's that then? And the hat. She's a witch! The Lady of the Lake, her arm clad in the purest shimmering samite, held aloft Excalibur from the bosom of the water, signifying by divine providence that I, Arthur, was to carry Excalibur. That is why I am your king. Be quiet!</p>
-<p>Ah, now we see the violence inherent in the system! Bring her forward! Well, she turned me into a newt. I am your king. Burn her anyway! It's only a model.</p>
     </div>
 </section>
