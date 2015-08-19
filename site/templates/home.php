016b44d09b86f1921759c161cd4ec4182998e413
Nathan Chapman <nathan@nchp.mn>
2015-08-19 21:59:34 +1000

    Add much testing content

--- site/templates/home.php
+++ site/templates/home.php
@@ -7,8 +7,70 @@
         <p class="splash-subhead">
             <?php echo $site->description() ?>
         </p>
-        <p>
-            <a href="http://purecss.io" class="pure-button pure-button-primary">Get Started</a>
-        </p>
+    </div>
+</div>
+
+<div class="content-wrapper">
+    <div class="content">
+        <div class="pure-g">
+            <div class="pure-u-1 pure-u-md-1-2">
+            Pure Grids use a specific font stack to ensure the greatest OS/browser compatibility, and by default grid units will have font-family: sans-serif; applied — this is the default font stack Pure's Base (Normalize.css) applies to Introduction to Pure Grids Pure Grids are easy to work with, and very powerful. There are a few simple concepts to keep in mind: Grid classes vs. unit classes Pure Grids consist of two types of classes: the grid class (pure-g) and unit classes (pure-u or pure-u-*) The widths of the units are fractions Units have various class names that represent their widths. For example, pure-u-1-2 has a width of 50%, whereas pure-u-1-5 would have a width of 20%. All child elements of a grid must be units Child elements contained within an element with a pure-g classname must be a grid unit with a pure-u or pure-u-* classname. Content goes inside grid units All content which is visible to people needs to be contained inside a grid unit. This ensures that the content will be rendered properly. When setting a font-family in your project, be sure to check out the section on using Grids with your font-family. Let's start with a simple example. Here's a grid with three columns: Want to just use Grids? Grids is part of the Pure CSS file. However, if you just want Grids and not the other modules, you can pull it down separately. Just include this
+            </div>
+            <div class="pure-u-1 pure-u-md-1-2">
+            YOUR SITE
+Home
+
+Tour
+
+Sign Up
+BIG BOLD TEXT
+
+LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT.
+
+GET STARTED
+
+EXCEPTEUR SINT OCCAECAT CUPIDATAT.
+
+ Get Started Quickly
+
+Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+
+ Responsive Layouts
+
+Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+
+ Modular
+
+Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+
+ Plays Nice
+
+Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.
+
+File Icons
+LABORIS NISI UT ALIQUIP.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+
+DOLORE MAGNA ALIQUA. UIS AUTE IRURE.
+
+
+Your Name
+
+Your Email
+
+Your Password
+Sign Up
+Contact Us
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+More Information
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+View the source of this layout to learn more. Made with love by the YUI Team.
+            </div>
+        </div>
     </div>
 </div>
