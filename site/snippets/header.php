6a54ffca8adfed970fadcd42b859f8d9d27f1376
Nathan Chapman <nathan@nchp.mn>
2015-08-19 22:04:43 +1000

    Oops

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+    <meta name="description" content="<?php echo $site->description()->html() ?>">
+    <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <meta name="description" content="<?php echo $site->description()->html() ?>">
-  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-
-  <?php echo css('modules/purecss/pure.min.css') ?>
-  <?php echo css('assets/css/main.min.css') ?>
+    <?php echo css('modules/purecss/pure.min.css') ?>
+    <?php echo css('assets/css/main.min.css') ?>
+    <?php echo css('assets/css/fonts/flama-font.min.css') ?>
 
 </head>
 <body>
